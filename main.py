b1d52f9e33b84c0c7cb1e271355b3d3e41d40adf
hemant-17 <56511499+hemant-17@users.noreply.github.com>
2020-03-01 15:08:56 +0530

    basic_regressor

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-import matplotlib as plt
+
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets , linear_model
+from sklearn.metrics import mean_squared_error
 
 diabetes = datasets.load_diabetes()
 
-#diabetes_x =diabetes.data
 #(['data', 'target', 'DESCR', 'feature_names', 'data_filename', 'target_filename'])
 
-print(diabetes.DESCR)
+#print(diabetes.DESCR)
+
+diabetes_x =diabetes.data
+
+diabetes_x_train = diabetes_x[:-30]
+diabetes_x_test = diabetes_x[-30:]
+
+diabetes_y_train = diabetes.target[:-30]
+diabetes_y_test = diabetes.target[-30:]
+
+model = linear_model.LinearRegression()
+
+model.fit(diabetes_x_train,diabetes_y_train)
+
+diabetes_y_predicted = model.predict(diabetes_x_test)
+
+print("Mean squared error is ",mean_squared_error(diabetes_y_test,diabetes_y_predicted))
+print("weights is ",model.coef_)
+print("intercepts is ",model.intercept_)
+
+#plt.scatter(diabetes_x_test,diabetes_y_test)
+#plt.plot(diabetes_x_test,diabetes_y_predicted)
+#plt.show()
